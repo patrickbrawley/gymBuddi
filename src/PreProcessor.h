6be5708ab91f358cec979b45145169382a6a7b98
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-21 15:13:27 +0000

    Create PreProcessor.h

---
+++ src/PreProcessor.h
@@ -0,0 +1,17 @@
+#include <opencv2/core.hpp>
+#include "Scene.h"
+#include "PipelineLink.h"
+#include "PreProcessorSettings.h"
+
+class PreProcessor : public PipelineLink{
+public:
+    PreProcessor();
+    PreProcessor(PreProcessorSettings settings);
+    void NextScene(Scene scene);
+    void SetBoundingBox(float upperLeftX, float upperLeftY, float lowerRightX, float lowerRightY);
+    
+private:
+    Scene drawBox(Scene s);
+    Scene switchRGB2BGR(Scene s);
+    PreProcessorSettings settings;
+};
