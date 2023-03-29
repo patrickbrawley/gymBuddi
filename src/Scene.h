540b71f6f98f6ec2cbe71a885d296a193c1dddaf
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-29 16:53:16 +0100

    comments added

--- src/Scene.h
+++ src/Scene.h
@@ -15,23 +15,28 @@ struct Point
 
 
 struct BoundingBox{
-    Point UpperLeft;
-    Point LowerRight;
+    Point UpperLeft; // The upper-left corner of the bounding box
+    Point LowerRight;  // The lower-right corner of the bounding box
+    // Default constructor initializes both points to (0, 0)
     BoundingBox():UpperLeft(), LowerRight(){}
     BoundingBox(int upperLeftX, int upperLeftY, int lowerRightX, int lowerRightY): UpperLeft(upperLeftX, upperLeftY),
                                                                                    LowerRight(lowerRightX, lowerRightY) {}
     BoundingBox(Point upperLeft, Point lowerRight): UpperLeft(upperLeft), LowerRight(lowerRight) {}
 };
 
-
+//Holds the data obtained in a single video frame to allow it to be processed and passed down the pipleline
 struct Scene{
     Scene(){}
-  
+    
+    //OpenCV n-dimensional dense array object representing the video frame
+
     cv::Mat frame;
   
+    // A string representing the predicted output from processing with a CNN model, such as "plank"
     std::string result = "";
-   
-     BoundingBox regionOfInterest;
+    
+   //the area of the scene that will be processed.
+     BoundingBox regionOfInterest; 
 };
 
 #endif
