d773246f29a714c374c9c75286130fba6c7b9daa
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-29 16:04:56 +0100

    comments

--- src/PreProcessor.h
+++ src/PreProcessor.h
@@ -3,6 +3,9 @@
 #include "PipelineLink.h"
 #include "PreProcessorSettings.h"
 
+
+//This class processes an input scene, then it is passed on to the next component in the pipeline.
+
 class PreProcessor : public PipelineLink{
 public:
     PreProcessor();
@@ -11,6 +14,7 @@ public:
     void SetBoundingBox(float upperLeftX, float upperLeftY, float lowerRightX, float lowerRightY);
     
 private:
+    //private member function that draws a box or rectangle on the input image based on the bounding box coordinates and converts the scene from RGB to BGR color format to allow the Neural Network to read the scene. 
     Scene drawBox(Scene s);
     Scene switchRGB2BGR(Scene s);
     PreProcessorSettings settings;
