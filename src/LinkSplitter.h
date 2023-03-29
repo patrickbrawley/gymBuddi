d603f1ab4bf74d06a961dbe88c754d7eb7b2a0fd
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-29 14:43:16 +0100

    comments

--- src/LinkSplitter.h
+++ src/LinkSplitter.h
@@ -3,12 +3,15 @@
 
 #include "PipelineLink.h"
 
-
+// Declare the LinkSplitter class which extends the PipelineLink. NextScene is copied and added to another element. 
 class LinkSplitter : public PipelineLink{
 public:
+    // Public method to handle the next scene in the pipeline
     void NextScene(Scene s);
+    // Public method to register a secondary callback
     void RegisterSecondaryCallback(SceneCallback* scb);
 public:
+    // Public member variable to store the secondary callback
     SceneCallback* secondarySceneCallback = nullptr;
     
 };
