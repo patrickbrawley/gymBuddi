320feef8721faa12fbeec4d3dc3fd9c3253ce54b
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-29 14:07:40 +0100

    updated comments

--- src/LinkSplitter.cpp
+++ src/LinkSplitter.cpp
@@ -1,11 +1,14 @@
 #include "LinkSplitter.h"
 
+// Implementation of the NextScene method
 
 void LinkSplitter::NextScene(Scene s) {
+     // Call the NextScene of the primary callback
     if(sceneCallback) sceneCallback->NextScene(s);
+     // Call the NextScene of the secondary callback
     if(secondarySceneCallback) secondarySceneCallback->NextScene(s);
 }
-
+// Implementation of the Secondary Callback method
 void LinkSplitter::RegisterSecondaryCallback(SceneCallback *scb) {
     secondarySceneCallback = scb;
 }
