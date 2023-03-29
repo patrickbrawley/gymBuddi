1a36e85567b1117f3fd26897cab959da92a60b8d
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-29 12:20:07 +0100

    comments

--- src/CNNProcessor.cpp
+++ src/CNNProcessor.cpp
@@ -1,20 +1,21 @@
 #include "CNNProcessor.h"
 
-
+// Load the trained TF model 
 void CNNProcessor::LoadModel(std::string modelPath){
     net = cv::dnn::readNetFromTensorflow(modelPath);
 }
 
 
+// Constructor that loads the trained TF model
 CNNProcessor::CNNProcessor(CNNProcessorSettings s) : settings(s) {
     LoadModel(settings.ModelPath);
 }
 
 
-
+// Implementation of the MakeBlob function that extracts a region of interest from a scene and converts it to a blob
 cv::Mat CNNProcessor::MakeBlob(Scene scene){
-    int x  = scene.regionOfInterest.UpperLeft.x; int y = scene.regionOfInterest.UpperLeft.y;
-    int width = scene.regionOfInterest.LowerRight.x - x;
+    int x  = scene.regionOfInterest.UpperLeft.x; int y = scene.regionOfInterest.UpperLeft.y; 
+    int width = scene.regionOfInterest.LowerRight.x - x; 
     int height = scene.regionOfInterest.LowerRight.y - y;
     cv::Mat roi = scene.frame(cv::Range(y, y+height), cv::Range(x, x+width));
     cv::Mat small;
@@ -25,15 +26,16 @@ cv::Mat CNNProcessor::MakeBlob(Scene scene){
 }
 
 
+// Implementation of the ProcessScene function that processes a scene using a trained CNN model
 Scene CNNProcessor::ProcessScene(Scene scene){
-    if(scene.frame.empty()) return scene;
-    cv::Mat blob = MakeBlob(scene);
-    net.setInput(blob);
-    cv::Mat prob = net.forward();
+    if(scene.frame.empty()) return scene; // If the scene is empty, return it without processing
+    cv::Mat blob = MakeBlob(scene); // Convert the scene to a blob
+    net.setInput(blob); // Set the input for the neural network
+    cv::Mat prob = net.forward(); // Forward propagate the blob through the network
     cv::Point classIdPoint;
     double confidence;
-    minMaxLoc(prob.reshape(1, 1), 0, &confidence, 0, &classIdPoint);
-    int classId = classIdPoint.x;
-    scene.result = gymBuddi::getPoseFromDigit(classId);
-    return scene;
+    minMaxLoc(prob.reshape(1, 1), 0, &confidence, 0, &classIdPoint);  // Find the class with the highest probability
+    int classId = classIdPoint.x; // Get the class ID
+    scene.result = gymBuddi::getPoseFromDigit(classId); // Set the desired pose as the result the frame will be searching for.
+    return scene; // Return the processed scene to pass along the pipeline
 }
