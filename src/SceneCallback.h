59fea75bbe52beb23f28da0faf13066f056cd3ab
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-31 10:00:23 +0100

    Update SceneCallback.h

--- src/SceneCallback.h
+++ src/SceneCallback.h
@@ -3,8 +3,14 @@
 
 #include "Scene.h"
 
+//this class provides a way to pass scenes through a pipleline to allow the camera stream to be processed by the Neural retwork and the other methods present in 'scene.h'
+
 class SceneCallback{
 public:
+    // NextScene is a pure virtual function, (becuase = 0) 
+    //this allows any class that implements NextScene to provide its own implementation for handling the Scene passed down the pipeline
+    //Nextscene ensures that the SceneCallback interface is used consistently throughout the video processing architecture, 
+    //and it allows for greater flexibility in how different parts of the pipeline can be implemented.
     virtual void NextScene(Scene next) = 0;
 };
 
