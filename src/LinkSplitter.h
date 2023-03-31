3d4814d1aef8e1b5bb7ab3c18cb79c2d9e254eb1
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-31 12:34:04 +0100

    updated comments
    code explained here

--- src/LinkSplitter.h
+++ src/LinkSplitter.h
@@ -3,7 +3,9 @@
 
 #include "PipelineLink.h"
 
-// Declare the LinkSplitter class which extends the PipelineLink. NextScene is copied and added to another element. 
+//subclass of PipelineLink and is used to split a pipeline into two separate pipelines by registering a secondary scene callback function. 
+//When a scene is passed through the NextScene function, it will be passed on to both the primary and secondary scene callback functions for further processing. 
+//This allows the same scene to be processed in two different ways at the same time.
 class LinkSplitter : public PipelineLink{
 public:
     // Public method to handle the next scene in the pipeline
