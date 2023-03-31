7a29a053abc5723c48782c20494f20f12c24bf17
patrickbrawley <122913837+patrickbrawley@users.noreply.github.com>
2023-03-31 11:07:40 +0100

    Update Timer.cpp

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -3,15 +3,17 @@
 #include "iostream"
 
 
-int Timer::GetProgress(std::string result, std::string task){
+int Timer::Gainz(std::string result, std::string task){
+    // if user is in the correct pose with good form the counter will count up. 
     if(task == result){
         count++;
     }
-    
+    //The count variable times of the user performing an excersise, and is returned as the output of the function.
     return count;
 }
 
-void Timer::ResetProgress(){
+void Timer::NewGainz(){
+    //this method resets the timer to zero. allows a new excerise to be performed or the user needs to start over.
     count = 0;
 }
 
